5c7cf4304d8db3e50ce0d78fdcbd18983eacb108
govardhanvithal974-hub <govardhanvithal974@gmail.com>
2026-05-15 08:56:35 +0530

    Update practice-queries.sql

--- week1/day1/practice-queries.sql
+++ week1/day1/practice-queries.sql
@@ -33,3 +33,58 @@
 
 -- Question 10
 -- Display department-wise employee count.
+--------------------------------------------------------
+-- Date Queries
+--------------------------------------------------------
+
+-- 11. Select employees hired in the year 2020.
+
+-- 12. Select employees hired in January of any year.
+
+-- 13. Select employees hired before 2019.
+
+-- 14. Select employees hired on or after March 1, 2021.
+
+-- 15. Select employees hired in the last 2 years.
+
+--------------------------------------------------------
+-- Aggregate Queries
+--------------------------------------------------------
+
+-- 16. Select the total salary of all employees.
+
+-- 17. Select the average salary of employees.
+
+-- 18. Select the minimum salary in the Employee table.
+
+-- 19. Select the number of employees in each department.
+
+-- 20. Select the average salary of employees in each department.
+
+--------------------------------------------------------
+-- Group By Queries
+--------------------------------------------------------
+
+-- 21. Select the total salary for each department.
+
+-- 22. Select the average age of employees in each department.
+
+-- 23. Select the number of employees hired in each year.
+
+-- 24. Select the highest salary in each department.
+
+-- 25. Select the department with the highest average salary.
+
+--------------------------------------------------------
+-- Having Queries
+--------------------------------------------------------
+
+-- 26. Select departments with more than 2 employees.
+
+-- 27. Select departments with an average salary greater than 55000.
+
+-- 28. Select years with more than 1 employee hired.
+
+-- 29. Select departments with a total salary expense less than 100000.
+
+-- 30. Select departments with the maximum salary above 75000.
